8e60a15e4185923b80da7ad6f2f5dcc7a3e8a183
Farhad Ali Abbasi <34246950+FarhadAbbasi@users.noreply.github.com>
2024-12-17 18:33:01 +0500

    Added a Home Image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import './Products.css' ;
 
 
 function Home() {
   return (
     <div>
         <h1>  This is a Shoe Store </h1>
-        <h4>  Find Shoes for Your Dream Adventure here... </h4>
+        <div className="home-img">
+          <h4>  Find Shoes for Your Dream Adventure here... </h4>
+         <img src='/NIKE1.jpg' alt="Shoe" ></img>
+        </div>
     </div>        
   );
 }
